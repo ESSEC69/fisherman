7a8acf75fd14f25946f11f64763d218a1637075a
olivierjonquet <jonqueto@hotmail.fr>
2016-05-17 17:51:47 +0200

    Adding profile and update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517164737) do
+ActiveRecord::Schema.define(version: 20160517164738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20160517164737) do
     t.datetime "token_expiry"
     t.string   "phone_number"
     t.string   "description"
+    t.string   "photo"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
