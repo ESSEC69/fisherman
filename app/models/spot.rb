b1729dbefcc3ae52ce8ba99665f74ae4292d8d94
louis steenbrink <louis.steenbrink@gmail.com>
2016-05-17 18:06:53 +0200

    added map to spot#show

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,16 @@
 class Spot < ActiveRecord::Base
   validates :title, presence: true
-
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
   belongs_to :user
+
+  def address
+    [street_number, street, postal_code, city].compact.join(" ")
+  end
+
+  def address_changed?
+    street_number_changed? || street_changed? || postal_code_change? || city_changed?
+  end
+
 end
--- db/migrate/20160517164738_add_address_to_spot.rb
+++
@@ -1,4 +0,0 @@
-class AddAddressToSpot < ActiveRecord::Migration
-  def change
-  end
-end
