9d582c1f2ee81c50423a4718d15da52f7cd2ad42
Cyrille Ducret <cyrille.cpi@gmail.com>
2016-05-17 10:33:29 -0500

    generate model et controller for spot

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516215130) do
+ActiveRecord::Schema.define(version: 20160517153027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "spots", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "price_per_day"
+    t.string   "street_number"
+    t.string   "street"
+    t.string   "postal_code"
+    t.string   "city"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "spots", ["user_id"], name: "index_spots_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +51,5 @@ ActiveRecord::Schema.define(version: 20160516215130) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "spots", "users"
 end
