26192b382d294be0cffd41d4b7b8fcfa0cc7f4bc
louis steenbrink <louis.steenbrink@gmail.com>
2016-05-18 15:01:00 +0200

    nothing done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @reviews = Review.joins(:booking).where("bookings.user_id = #{@user.id} and reviews.ownership = true")
   end
 end
