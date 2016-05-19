7ace590c19bbc416000967729f18afb7562dd535
louis steenbrink <louis.steenbrink@gmail.com>
2016-05-19 16:04:30 +0200

    facebook connect is working and modal for sign up/log in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+  def resource_name
+
+  :user
+
+  end
+
+  def resource
+
+  @resource ||= User.new
+
+  end
+
+  def provider
+    :facebook
+  end
+
+  def devise_mapping
+
+  @devise_mapping ||= Devise.mappings[:user]
+
+  end
+
 end
