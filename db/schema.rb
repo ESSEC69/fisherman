cd89846a9f9a81535367b9c87cec80919df9d099
olivierjonquet <jonqueto@hotmail.fr>
2016-05-20 15:40:20 +0200

    chat-form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519211711) do
+ActiveRecord::Schema.define(version: 20160520090638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20160519211711) do
 
   add_index "bookings", ["spot_id"], name: "index_bookings_on_spot_id", using: :btree
   add_index "bookings", ["user_id"], name: "index_bookings_on_user_id", using: :btree
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "reviews", force: :cascade do |t|
     t.text     "content"
@@ -92,6 +101,7 @@ ActiveRecord::Schema.define(version: 20160519211711) do
 
   add_foreign_key "bookings", "spots"
   add_foreign_key "bookings", "users"
+  add_foreign_key "messages", "users"
   add_foreign_key "reviews", "bookings"
   add_foreign_key "reviews", "users"
   add_foreign_key "spots", "users"
